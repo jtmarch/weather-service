be6c6a2545fa38c41b543669d9358bcb1d32753f
jtmarch <jelle@4family.nl>
2023-10-02 12:00:20 +0200

    first commit

---
+++ src/main/java/com/example/demo/WeatherController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/weather")
+public class WeatherController {
+
+    private final WeatherRepository weatherRepository;
+
+    public WeatherController(WeatherRepository weatherRepository) {
+        this.weatherRepository = weatherRepository;
+    }
+
+    @GetMapping("/city")
+    public Optional<Weather> getWeatherForCity(@RequestParam("name") String cityName) {
+        return weatherRepository.findById(cityName);
+    }
+}
